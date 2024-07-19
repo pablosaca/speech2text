66b8541fb17778ea68b1703fc27a777ca3eff0dd
p_san <psancabrera@gmail.com>
2024-07-19 22:30:30 +0200

    anadir ficheros base y transcripción de video escena película "conoces a joe-black"

---
+++ script_3.py
@@ -0,0 +1,23 @@
+import logging
+import whisper
+import pandas as pd
+
+# Configuración básica del logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+model = whisper.load_model('large')  # cargamos el modelo grande de Whisper
+logger.info("Modelo whisper cargado")
+
+# como sabemos que el audio está en castellano indicamos el lenguaje
+response = model.transcribe(
+    audio="audios/youtube/escena_cafeteria.mp3",
+    language="es",
+    verbose=True
+)
+logger.info("Transcripción realizada")
+
+d = {"texto": response["text"]}
+df = pd.DataFrame(d, index=[0])
+
+df.to_csv("textos/conoces_joe_black.csv")
